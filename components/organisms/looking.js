91d19019623b2bd6678162d32b862efcaaf1b6a0
danteclericuzio <dante.clericuzio@fintopia.tech>
2025-03-13 23:47:47 +0700

    category done UI

--- components/organisms/looking.js
+++ components/organisms/looking.js
@@ -2,7 +2,7 @@
  * @Author: danteclericuzio
  * @Date: 2025-03-13 13:17:29
  * @Last Modified by: danteclericuzio
- * @Last Modified time: 2025-03-13 21:26:01
+ * @Last Modified time: 2025-03-13 22:06:05
  */
 
 import { OrangeText, TitleText } from '@/components/atoms/title';
@@ -12,15 +12,15 @@ import { Home } from '@/constants/en';
 export default function Looking() {
     const { looking } = Home;
     return (
-        <div className="lingo-container pt-[100px] flex flex-col relative overflow-hidden">
-            <div className='bg-gradient-to-b blur-lg from-pink-50 rounded-full to-white h-[550px] w-[550px] absolute left-[50px] md:left-[250px] top-[200px]'></div>
-            <div className='bg-gradient-to-b blur-lg from-purple-100 rounded-full to-white h-[550px] w-[550px] absolute left-[200px] md:left-[550px] top-[450px]'></div>
+        <div className="lingo-container pt-[100px] flex flex-col relative">
+            <div className='bg-gradient-to-b blur-lg from-pink-50 rounded-full to-white h-[550px] w-[150px] sm:w-[250px] lg:w-[550px] absolute left-[50px] md:left-[250px] top-[200px]'></div>
+            <div className='bg-gradient-to-b blur-lg from-purple-100 rounded-full to-white h-[550px] w-[150px] sm:w-[250px] lg:w-[450px] absolute left-[100px] md:left-[300px] lg:left-[450px] top-[450px]'></div>
             <OrangeText text={looking.title} position="justify-center"/>
 
             <div className="w-full flex justify-center flex-col">
                 <TitleText text={looking.subtitle}/>
                 <div className="flex flex-col md:flex-row gap-[36px]">
-                    <div className="items-center flex flex-col w-full md:h-[455px] py-[50px] px-[46px] drop-shadow-xl bg-[#FFFFFF] rounded-[10px] animation-effect">
+                    <div className="items-center flex flex-col w-full md:h-[455px] py-[30px] md:py-[50px] px-[15px] md:px-[46px] drop-shadow-xl bg-[#FFFFFF] rounded-[10px] animation-effect">
                         <img src='/assets/teach.png' alt='teacher' className="w-[96px] h-[100px] mx-auto"/>
                         <span 
                         className="
@@ -34,7 +34,7 @@ export default function Looking() {
                         <span className='text-[#707070] text-center mb-[32px]'>{looking.teachDesc}</span>
                         <WhiteButton text={looking.registerButton}/>
                     </div>
-                    <div className="items-center flex flex-col w-full md:h-[455px] py-[50px] px-[46px] drop-shadow-xl bg-[#FFFFFF] rounded-[10px] animation-effect">
+                    <div className="items-center flex flex-col w-full md:h-[455px] py-[30px] md:py-[50px] px-[15px] md:px-[46px] drop-shadow-xl bg-[#FFFFFF] rounded-[10px] animation-effect">
                         <img src='/assets/learn.png' alt='teacher' className="w-[96px] h-[100px] mx-auto"/>
                         <span 
                         className="
