e4f92986f99fc6d7c13c5336a41d484cca395ecc
danteclericuzio <dante.clericuzio@fintopia.tech>
2025-03-15 17:35:02 +0700

    hero bottom done UI

---
+++ components/organisms/hero-bottom.js
@@ -0,0 +1,65 @@
+/*
+ * @Author: danteclericuzio
+ * @Date: 2025-03-15 16:47:13
+ * @Last Modified by: danteclericuzio
+ * @Last Modified time: 2025-03-15 17:34:18
+ */
+
+import { OrangeButton } from "@/components/atoms/buttons";
+import { TitleText } from "@/components/atoms/title";
+import { Home } from "@/constants/en";
+
+export default function HeroBottom() {
+    const { heroBottom } = Home;
+    return (
+        <div className="lingo-container py-[50px] md:py-[115px]">
+            <div className="block lg:hidden mb-[20px]">
+                <TitleText text={heroBottom.title}/>
+            </div>
+            <div className="animation-effect text-[14px] sm:text-[16px] md:text-[18px] flex lg:hidden flex-col mb-[20px] ">
+                <ol className="list-decimal list-inside">
+                {heroBottom.list.map((item, idx) => (
+                    <li key={idx}>{item}</li>
+                ))}
+                </ol>
+            </div>
+            <div 
+                className="
+                animation-effect
+                relative
+                w-full
+                h-[200px] md:h-[500px] xl:h-[800px]
+                bg-[url('/assets/hero-bottom.png')]
+                bg-cover
+                bg-no-repeat
+                bg-
+                rounded-[25px] md:rounded-[50px]
+                "
+            >   
+                <div className="animation-effect p-[50px] hidden lg:flex flex-col absolute w-[540px] h-[400px] bg-[#F9E8E2] lg:bottom-[30px] xl:bottom-[100px] right-[30px]">
+                        <TitleText text={heroBottom.title}/>
+                        <ol className="list-decimal list-inside my-[20px]">
+                            {heroBottom.list.map((item, idx) => (
+                                <li key={idx}>{item}</li>
+                            ))}
+                        </ol>
+                        <div>
+                            <OrangeButton text={heroBottom.button} />
+                        </div>
+                </div>
+                <div className="relative w-full h-full md:hidden">
+                    <div className="
+                        absolute
+                        top-1/2
+                        left-1/2
+                        -translate-x-1/2
+                        -translate-y-1/2
+                    ">
+                        <OrangeButton text={heroBottom.button} />
+                    </div>
+                    <div className="w-full h-full bg-[#00000060] rounded-[25px]"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- components/pages/home.js
+++ components/pages/home.js
@@ -2,7 +2,7 @@
  * @Author: danteclericuzio
  * @Date: 2025-03-13 21:27:45
  * @Last Modified by: danteclericuzio
- * @Last Modified time: 2025-03-14 21:11:31
+ * @Last Modified time: 2025-03-15 16:48:29
  */
 
 import Browse from '@/components/organisms/browse';
@@ -13,6 +13,7 @@ import ExploreTutor from '@/components/organisms/explore-tutor';
 import Why from '@/components/organisms/why-us';
 import How from '@/components/organisms/how';
 import Review from '@/components/organisms/review';
+import HeroBottom from '@/components/organisms/hero-bottom';
 
 export default function Home() {
     return (
@@ -25,6 +26,7 @@ export default function Home() {
             <Why/>
             <How/>
             <Review/>
+            <HeroBottom/>
         </>
     )
 }
