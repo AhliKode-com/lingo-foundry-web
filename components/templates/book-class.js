39825ac08552dc98fa4f1369f5cc934fd1fc4ae4
dclericuzio <danteclericuzio1999@gmail.com>
2025-03-18 00:41:06 +0700

    new component multi step in testing route

---
+++ components/templates/book-class.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: danteclericuzio
+ * @Date: 2025-03-17 23:50:17
+ * @Last Modified by: danteclericuzio
+ * @Last Modified time: 2025-03-17 23:51:15
+ */
+
+export default function BookClass() {
+    return (
+        <div>
+            <h2 className="font-bold text-xl">Book Class</h2>
+            <p className="mt-2">This is where you confirm your payment details.</p>
+            {/* ...the rest of your content */}
+        </div>
+    )
+}
