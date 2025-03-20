f1887f7dbb6092dae006f525829c8f21d6997b53
Advis Tasyah Mulia <advis.mulia@shopee.com>
2025-03-20 10:37:16 +0700

    feat: login page ui

---
+++ app/login/page.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: advistasyam
+ * @Date: 2025-03-13 13:04:54
+ * @Last Modified by: advistasyam
+ * @Last Modified time: 2025-03-16 18:39:09
+ */
+
+import LoginPage from '@/components/pages/login';
+
+export default function Login() {
+    return (
+        <LoginPage/>
+    )
+}
