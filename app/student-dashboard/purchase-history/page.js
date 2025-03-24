1d4fc12ddd418e76b61e1fed89e3355815198994
ahlikode <dev@ahlikode.com>
2025-03-24 09:52:24 +0700

    student navbar done UI (#30)
    Co-authored-by: dclericuzio <danteclericuzio1999@gmail.com>

---
+++ app/student-dashboard/purchase-history/page.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: danteclericuzio
+ * @Date: 2025-03-17 23:52:57
+ * @Last Modified by: danteclericuzio
+ * @Last Modified time: 2025-03-24 09:23:21
+ */
+
+export default function StudentPurchaseHistory() {
+    return (
+        <div>STUDENT purchase history</div>
+    )
+}
