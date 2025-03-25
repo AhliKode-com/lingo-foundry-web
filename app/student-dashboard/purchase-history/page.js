9fc559867469a77dde36a3b62c01f7f97dbcda20
ahlikode <dev@ahlikode.com>
2025-03-25 16:19:21 +0700

    purchase history done ui responsive (#34)
    Co-authored-by: dclericuzio <danteclericuzio1999@gmail.com>

--- app/student-dashboard/purchase-history/page.js
+++ app/student-dashboard/purchase-history/page.js
@@ -2,11 +2,13 @@
  * @Author: danteclericuzio
  * @Date: 2025-03-17 23:52:57
  * @Last Modified by: danteclericuzio
- * @Last Modified time: 2025-03-24 09:23:21
+ * @Last Modified time: 2025-03-25 14:12:38
  */
+
+import StudentPurchasingHistory from '@/components/pages/student-purchasing-history';
 
 export default function StudentPurchaseHistory() {
     return (
-        <div>STUDENT purchase history</div>
+        <StudentPurchasingHistory/>
     )
 }
