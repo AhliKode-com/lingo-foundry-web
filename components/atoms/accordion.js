eac86ee4062a2542a17e42a62992e7c334692d73
ahlikode <dev@ahlikode.com>
2025-04-06 13:29:14 +0700

    integration: dashboard purchase history (#50)
    Co-authored-by: advistasyam <advis.tasyah@ui.ac.id>

--- components/atoms/accordion.js
+++ components/atoms/accordion.js
@@ -5,11 +5,11 @@
  * @Last Modified time: 2025-03-25 19:53:17
  */
 "use client"
-import React, { useState, useRef, useEffect } from "react";
-import { FiChevronDown } from "react-icons/fi";
-import { GoArrowUp } from "react-icons/go";
+import React, {useState, useRef, useEffect} from "react";
+import {FiChevronDown} from "react-icons/fi";
+import {GoArrowUp} from "react-icons/go";
 
-export function Question ({ title, answer, defaultOpen = false }) {
+export function Question({title, answer, defaultOpen = false}) {
     const [open, setOpen] = useState(defaultOpen);
     const [height, setHeight] = useState(0);
     const contentRef = useRef(null);
@@ -22,9 +22,9 @@ export function Question ({ title, answer, defaultOpen = false }) {
                 setHeight(0);
             }
         }
-    }, [open, answer]); 
-    
-      return (
+    }, [open, answer]);
+
+    return (
         <div className="w-full gap-0">
             <button
                 onClick={() => setOpen((prev) => !prev)}
@@ -50,7 +50,7 @@ export function Question ({ title, answer, defaultOpen = false }) {
                 >
                     {title}
                 </span>
-                
+
                 <div>
                     <FiChevronDown
                         className={`
@@ -62,7 +62,7 @@ export function Question ({ title, answer, defaultOpen = false }) {
                     />
                 </div>
             </button>
-    
+
             <div
                 className="
                     text-[12px] sm:text-[14px]
@@ -75,7 +75,7 @@ export function Question ({ title, answer, defaultOpen = false }) {
                     height: height
                 }}
             >
-                <div 
+                <div
                     ref={contentRef}
                     className="py-[20px] px-[24px] bg-[#F9F9F9] border-[1px] border-[#E9EAF0]"
                 >
@@ -83,24 +83,30 @@ export function Question ({ title, answer, defaultOpen = false }) {
                 </div>
             </div>
         </div>
-      );
+    );
 }
 
-export function PurchaseHistory({data, defaultOpen = false}){
+export function PurchaseHistory({data, defaultOpen = false}) {
     const [open, setOpen] = useState(defaultOpen);
     const [height, setHeight] = useState(0);
     const contentRef = useRef(null);
 
+    const createdAt = data?.transactions[0]?.createdAt;
+    let totalAmount = 0;
+    for (const transaction of data.transactions) {
+        totalAmount += transaction.totalAmount;
+    }
+
     useEffect(() => {
         if (contentRef.current) {
-        if (open) {
-            setHeight(contentRef.current.scrollHeight);
-        } else {
-            setHeight(0);
+            if (open) {
+                setHeight(contentRef.current.scrollHeight);
+            } else {
+                setHeight(0);
+            }
         }
-        }
-    }, [open, 'aaa']); 
-      
+    }, [open]);
+
     return (
         <div className="w-full gap-0">
             <button
@@ -125,22 +131,27 @@ export function PurchaseHistory({data, defaultOpen = false}){
                             ${open ? "text-[#E35D33]" : "text-[#1D2026]"}
                         `}
                     >
-                        {data.date}
+                        {createdAt}
                     </span>
                     <div className="flex flex-col sm:flex-row sm:items-center gap-[8px] sm:gap-[16px] animation-effect">
                         <div className="flex items-center gap-[6px]">
-                            <img src="/assets/play.svg" alt="coin" className="animation-effect w-[16px] h-[16px] md:w-[20px] md:h-[20px]"/>
-                            <span className="animation-effect text-[12px] md:text-[14px] text-[#4E5566]">{data.courseCount} Courses</span>
+                            <img src="/assets/play.svg" alt="coin"
+                                 className="animation-effect w-[16px] h-[16px] md:w-[20px] md:h-[20px]"/>
+                            <span
+                                className="animation-effect text-[12px] md:text-[14px] text-[#4E5566]">{data.courses.length} Courses</span>
                         </div>
-                        <span className="flex animation-effect text-[12px] md:text-[14px] text-[#4E5566]">Rp.{Number(data.totalPrice).toLocaleString('id-ID')}</span>
-                        <div className="flex items-center gap-[6px]">
-                            <img src="/assets/cc.svg" alt="coin" className="animation-effect w-[16px] h-[16px] md:w-[20px] md:h-[20px]"/>
-                            <span className="animation-effect text-[12px] md:text-[14px] text-[#4E5566]">{data.paymentMethod}</span>
-                        </div>
+                        <span
+                            className="flex animation-effect text-[12px] md:text-[14px] text-[#4E5566]">Rp.{Number(totalAmount).toLocaleString('id-ID')}</span>
+                        {/*<div className="flex items-center gap-[6px]">*/}
+                        {/*    <img src="/assets/cc.svg" alt="coin"*/}
+                        {/*         className="animation-effect w-[16px] h-[16px] md:w-[20px] md:h-[20px]"/>*/}
+                        {/*    <span*/}
+                        {/*        className="animation-effect text-[12px] md:text-[14px] text-[#4E5566]">{data.paymentMethod}</span>*/}
+                        {/*</div>*/}
                     </div>
                 </div>
-                
-                <div 
+
+                <div
                     className={`
                         w-[38px] h-[38px] md:h-[48px] md:w-[48px] flex items-center justify-center
                         ${open ? "bg-[#E35D33]" : "bg-[#F5F7FA]"}
@@ -157,7 +168,7 @@ export function PurchaseHistory({data, defaultOpen = false}){
                     />
                 </div>
             </button>
-    
+
             <div
                 className="
                     text-[12px] sm:text-[14px]
@@ -169,13 +180,14 @@ export function PurchaseHistory({data, defaultOpen = false}){
                     height: height
                 }}
             >
-                <div 
+                <div
                     ref={contentRef}
                     className="py-[30px] px-[40px] bg-[#FFFFFF] border-[1px] border-[#E9EAF0] border-t-0"
                 >
                     {data.courses.map((item, index) => {
-                        return(
-                            <div key={index} className="animation-effect grid-cols-1 grid sm:grid-cols-2 lg:grid-cols-6 gap-[8px] md:gap-[20px] border-b-[1px] last:border-b-0 border-[#C1C8D0] py-[24px] md:py-[36px]">
+                        return (
+                            <div key={index}
+                                 className="animation-effect grid-cols-1 grid sm:grid-cols-2 lg:grid-cols-6 gap-[8px] md:gap-[20px] border-b-[1px] last:border-b-0 border-[#C1C8D0] py-[24px] md:py-[36px]">
                                 <div className="sm:col-span-2 animation-effect">
                                     <div className="flex items-center gap-[10px] md:gap-[20px] animation-effect">
                                         <img
@@ -184,38 +196,48 @@ export function PurchaseHistory({data, defaultOpen = false}){
                                             className="rounded-full w-[50px] h-[50px] lg:w-[80px] lg:h-[80px] block animation-effect"
                                         />
                                         <div className="flex flex-col gap-[4px] md:gap-[8px] animation-effect">
-                                            <div className="flex items-center gap-[2px] md:gap-[6px] animation-effect">
-                                                <img
-                                                    src="/assets/star-review.svg"
-                                                    alt="star"
-                                                    className="rounded-full w-[12px] h-[12px] md:w-[18px] md:h-[18px] animation-effect"
-                                                />
-                                                <span className="text-[#1D2026] text-[10px] md:text-[14px] font-medium animation-effect">{item.rating}</span>
-                                                <span className="text-[#8C94A3] text-[10px] md:text-[14px] font-medium animation-effect">({Number(item.reviewCount).toLocaleString('id-ID')})</span>
-                                            </div>
-                                            <span className="text-[10px] lg:text-[16px] animation-effect">{item.tutorName}</span>
+                                            {/*TODO: ask courses[].rating and courses[].reviewCount to BE*/}
+                                            {/*<div className="flex items-center gap-[2px] md:gap-[6px] animation-effect">*/}
+                                            {/*    <img*/}
+                                            {/*        src="/assets/star-review.svg"*/}
+                                            {/*        alt="star"*/}
+                                            {/*        className="rounded-full w-[12px] h-[12px] md:w-[18px] md:h-[18px] animation-effect"*/}
+                                            {/*    />*/}
+                                            {/*    <span*/}
+                                            {/*        className="text-[#1D2026] text-[10px] md:text-[14px] font-medium animation-effect">{item.rating}</span>*/}
+                                            {/*    <span*/}
+                                            {/*        className="text-[#8C94A3] text-[10px] md:text-[14px] font-medium animation-effect">({Number(item.reviewCount).toLocaleString('id-ID')})</span>*/}
+                                            {/*</div>*/}
+                                            <span
+                                                className="text-[10px] lg:text-[16px] animation-effect">{`${item.tutorFirstName} ${item.tutorLastName}`}</span>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="sm:col-span-2 flex items-center animation-effect">
                                     <div className="flex flex-col gap-[4px]">
                                         <div className="flex gap-[25px] items-center">
-                                            <span className="text-[#A1A5B3] text-[12px] md:text-[14px] w-[50px] md:w-[70px] animation-effect">Course:</span>
-                                            <span className="text-[##4E5566] text-[12px] md:text-[14px] animation-effect">{item.subjectLevel}</span>
+                                            <span
+                                                className="text-[#A1A5B3] text-[12px] md:text-[14px] w-[50px] md:w-[70px] animation-effect">Course:</span>
+                                            <span
+                                                className="text-[##4E5566] text-[12px] md:text-[14px] animation-effect">{item.subjectLevel}</span>
                                         </div>
                                         <div className="flex gap-[25px] items-center">
-                                            <span className="text-[#A1A5B3] text-[12px] md:text-[14px] w-[50px] md:w-[70px] animation-effect">Sessions:</span>
-                                            <span className="text-[##4E5566] text-[12px] md:text-[14px] animation-effect">{item.sessionCount}</span>
+                                            <span
+                                                className="text-[#A1A5B3] text-[12px] md:text-[14px] w-[50px] md:w-[70px] animation-effect">Sessions:</span>
+                                            <span
+                                                className="text-[##4E5566] text-[12px] md:text-[14px] animation-effect">{item.sessionCount}</span>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="flex lg:justify-center items-center text-[#E35D33] font-medium text-[14px] md:text-[20px] animation-effect">Rp.{Number(item.price).toLocaleString('id-ID')}</div>
-                                <div className={`
-                                    flex lg:justify-center items-center font-medium text-[14px] md:text-[20px] animation-effect
-                                    ${item.status === 'On Going' ? 'text-[#43B7A0]' : 'text-[#1D2026]'}
-                                `}>
-                                        {item.status}
-                                </div>
+                                {/* TODO: ask courses.price and courses.status in BE */}
+                                {/*<div*/}
+                                {/*    className="flex lg:justify-center items-center text-[#E35D33] font-medium text-[14px] md:text-[20px] animation-effect">Rp.{Number(item.price).toLocaleString('id-ID')}</div>*/}
+                                {/*<div className={`*/}
+                                {/*    flex lg:justify-center items-center font-medium text-[14px] md:text-[20px] animation-effect*/}
+                                {/*    ${item.status === 'On Going' ? 'text-[#43B7A0]' : 'text-[#1D2026]'}*/}
+                                {/*`}>*/}
+                                {/*    {item.status}*/}
+                                {/*</div>*/}
                             </div>
                         )
                     })}
