add1491251dbb45dab542c53b28eeb4e92d3547e
ahlikode <dev@ahlikode.com>
2025-04-29 15:13:35 +0700

    fix cart discount display (#88)
    Co-authored-by: dclericuzio <danteclericuzio1999@gmail.com>

--- components/atoms/cart-row.js
+++ components/atoms/cart-row.js
@@ -2,7 +2,7 @@
  * @Author: danteclericuzio
  * @Date: 2025-03-19 11:15:09
  * @Last Modified by: danteclericuzio
- * @Last Modified time: 2025-04-15 00:45:58
+ * @Last Modified time: 2025-04-29 15:09:37
  */
 
 import Image from "next/image";
@@ -16,9 +16,9 @@ export function CartRow({ item, onDelete }) {
 
   return (
     <div className="gap-[10px] relative grid grid-cols-1 md:grid-cols-6 lg:grid-cols-6 border-b-[2px] last:border-b-0 border-[#D2D2D250]">
-      <div className="py-[5px] md:py-[20px] lg:col-span-2 flex items-center gap-[30px] md:gap-[12px]">
+      <div className="mt-[20px] md:mt-0 py-[5px] md:py-[20px] lg:col-span-1 flex items-center gap-[30px] md:gap-[12px]">
         <TitlePayment text={confirmPayment.product} custom="w-[80px] md:hidden"/>
-        <div className="flex items-center gap-[12px]">
+        <div className="flex md:flex-col items-center gap-[12px]">
           <img
             src={item.tutorProfileUrl || "placeholder.svg"}
             alt="tutor-profile"
@@ -41,16 +41,33 @@ export function CartRow({ item, onDelete }) {
         <TitlePayment text={confirmPayment.sessions} custom="w-[80px] md:hidden"/>
         <span className={cellClass}>{item.sessionCount}{" "}Sessions</span>
       </div>
+      <div className="py-[20px] flex items-center gap-[30px] md:gap-0">
+        <TitlePayment text={confirmPayment.discount} custom="w-[80px] md:hidden"/>
+        <span className={cellClass}>{item.discount.split(".")[0]}{" "}%</span>
+      </div>
       <div className="flex items-center gap-[30px] md:gap-0 mb-[30px] md:mb-0">
         <TitlePayment text={confirmPayment.subTotal} custom="w-[80px] md:hidden"/>
-        <span className="text-[12px] lg:text-[16px] ext-[#484848] font-bold">Rp.{Number(item.totalPrice).toLocaleString('id-ID')}</span>
+        {item.discount !== "0" ? (
+          <div className="flex flex-col">
+            <span className="text-[12px] lg:text-[16px] text-[#9c9c9c] font-bold line-through">Rp.{Number(item.totalPrice).toLocaleString('id-ID')}</span>
+            <span className="text-[12px] lg:text-[16px] text-[#484848] font-bold">Rp.{Number(item.finalPrice).toLocaleString('id-ID')}</span>
+          </div>
+        ): 
+          <span className="text-[12px] lg:text-[16px] text-[#484848] font-bold">Rp.{Number(item.totalPrice).toLocaleString('id-ID')}</span>
+        }
       </div>
+      <button
+          onClick={() => onDelete(item.orderItemId)}
+          className={`bg-red-500 text-[14px] mb-[50px] py-[13px] text-white animation-effect md:hidden block`}
+      >
+        Delete from cart
+      </button>
       <Image
         src="/assets/delete.svg"
         alt="delete"
         height={15}
         width={15}
-        className="right-[0px] top-[53px] md:top-[53px] absolute cursor-pointer"
+        className="right-[0px] top-[30px] lg:top-[70px] absolute cursor-pointer md:block hidden"
         onClick={() => onDelete(item.orderItemId)}
       />
     </div>
