4ccce0fd1fc040bc6522f0f2747a7aaa3369b266
ahlikode <dev@ahlikode.com>
2025-05-21 23:13:45 +0700

    fix api change for tutor detail review bar part (#109)
    Co-authored-by: dclericuzio <danteclericuzio1999@gmail.com>

--- components/atoms/rating-summary.js
+++ components/atoms/rating-summary.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export function RatingSummary({averageRating,totalReviews,ratingCounts,dataReview}) {
+export function RatingSummary({averageRating,totalReviews,dataReview,ratingCounts}) {
     const roundedRating = Math.round(averageRating)
 
     const [showAll, setShowAll] = useState(false)
@@ -17,7 +17,7 @@ export function RatingSummary({averageRating,totalReviews,ratingCounts,dataRevie
                             <img src="/assets/star-review.svg" alt="Star" key={i} className="w-[24px] h-[24px]" />
                         ))}
                     </div>
-                    <span className="text-[14px]">{dataReview.length} reviews</span>
+                    <span className="text-[14px]">{totalReviews} reviews</span>
                 </div>
 
                 {/* Right section - Rating distribution */}
@@ -30,15 +30,15 @@ export function RatingSummary({averageRating,totalReviews,ratingCounts,dataRevie
                         <span className="text-[14px]">{rating}</span>
                         <div className="relative flex-1 h-[10px] border-[2px] border-[#E35D33]  overflow-hidden">
                             {count > 0 && (
-                            <div
-                                className="absolute top-0 left-0 h-full bg-[#E35D33] animation-effect"
-                                style={{
-                                width: `${(count / totalReviews) * 100}%`,
-                                }}
-                            />
+                                <div
+                                    className="absolute top-0 left-0 h-full bg-[#E35D33] animation-effect"
+                                    style={{
+                                    width: `${(count / totalReviews) * 100}%`,
+                                    }}
+                                />
                             )}
                         </div>
-                            <span className="w-12 text-right text-gray-500">({count})</span>
+                            <span className={`${count > 0 ? "text-[#E35D33]" : "text-gray-500"} w-12 text-center`}>({count})</span>
                         </div>
                     )
                     })}
@@ -46,27 +46,35 @@ export function RatingSummary({averageRating,totalReviews,ratingCounts,dataRevie
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 mt-[32px] gap-[40px] animation-effect">
-                {displayedReviews.length === 0 && (
+                {displayedReviews.length > 0 ? (
+                    displayedReviews.map((item, index) => (
+                        <div key={index} className="flex flex-col animation-effect">
+                            <div className="flex items-center gap-[16px]">
+                                <img src={item.photoUrl || "/placeholder.svg"} alt="img" className="h-[48px] w-[48px]"/>
+                                <div className="flex flex-col justify-between">
+                                    <span className="text-[14px] font-semibold">{item.author}</span>
+                                    <span className="text-[14px]">
+                                    {new Date(item.createdAt).toLocaleDateString("en-US", {
+                                        year: "numeric",
+                                        month: "long",
+                                        day: "numeric"
+                                    })}
+                                    </span>
+                                </div>
+                            </div>
+                            <div className="flex mt-[16px] mb-[8px]">
+                                {Array(item.rating).fill().map((_, i) => (
+                                    <img src="/assets/star-review.svg" alt="Star" key={i} className="w-[18px] h-[18px]" />
+                                ))}
+                            </div>
+                            <span className="">{item.description}</span>
+                        </div>
+                    ))
+                ): (
                     <div>No Reviews yet</div>
                 )}
-                {displayedReviews.map((review, index) => (
-                    <div key={index} className="flex flex-col animation-effect">
-                        <div className="flex items-center gap-[16px]">
-                            <img src="/assets/man-1.png" alt="img" className="h-[48px] w-[48px]"/>
-                            <div className="flex flex-col justify-between">
-                                <span className="text-[14px] font-semibold">{review.name}</span>
-                                <span className="text-[14px]">{review.date}</span>
-                            </div>
-                        </div>
-                        <div className="flex mt-[16px] mb-[8px]">
-                            {Array(review.rating).fill().map((_, i) => (
-                                <img src="/assets/star-review.svg" alt="Star" key={i} className="w-[18px] h-[18px]" />
-                            ))}
-                        </div>
-                        <span className="">{review.review}</span>
-                    </div>
-                ))}
             </div>
+            {/* BLM KE BWH */}
             {dataReview.length > 6 && (
                 <button
                     onClick={() => setShowAll(!showAll)}
