b2187a8c2a1f6fe1f59b2fb6cba11f9597cfdbd4
ahlikode <dev@ahlikode.com>
2025-10-01 02:24:51 +0700

    add suspense (#148)
    Co-authored-by: dclericuzio <danteclericuzio1999@gmail.com>

--- components/pages/check-certificate-validity.js
+++ components/pages/check-certificate-validity.js
@@ -2,15 +2,18 @@
  * @Author: danteclericuzio
  * @Date: 2025-03-16 18:38:38
  * @Last Modified by: danteclericuzio
- * @Last Modified time: 2025-09-30 15:36:35
+ * @Last Modified time: 2025-10-01 02:24:13
  */
 
 import HeroCheckValidity from "@/components/organisms/hero-check-certificate-validity";
+import {Suspense} from "react";
 
 export default function FindTutor() {
     return (
         <>
+        <Suspense fallback={null}>
             <HeroCheckValidity/>
+        </Suspense>
         </>
     )
 }
